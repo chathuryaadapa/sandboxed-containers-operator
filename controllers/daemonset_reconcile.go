66c7bc06b5abb9f49b962d6abb96355387a64798
root <root@kata-initrd-s390x1.fyre.ibm.com>
2026-01-06 02:12:54 -0800

    feat: update rpm-install image
    Signed-off-by: root <root@kata-initrd-s390x1.fyre.ibm.com>

--- controllers/daemonset_reconcile.go
+++ controllers/daemonset_reconcile.go
@@ -34,7 +34,7 @@ const (
 	rhelCoreOsExtensionsImageName = "rhel-coreos-extensions"
 	cliImageName                  = "cli"
 
-	daemonSetImage = "quay.io/openshift_sandboxed_containers/osc-daemonset:1.11"
+	daemonSetImage = "quay.io/cadapa/openshift_sandboxed_containers/osc-daemonset:main-latest"
 )
 
 // KataInstallationDaemonSetState defines the possible states of the Kata installation DaemonSet.

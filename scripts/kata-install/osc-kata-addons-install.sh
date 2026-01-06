94ee1042fb804ac27e4653fb3bc9e7af1df17296
root <root@kata-initrd-s390x1.fyre.ibm.com>
2026-01-06 01:04:19 -0800

    fix: backup
    Signed-off-by: root <root@kata-initrd-s390x1.fyre.ibm.com>

--- scripts/kata-install/osc-kata-addons-install.sh
+++ scripts/kata-install/osc-kata-addons-install.sh
@@ -112,9 +112,9 @@ uninstall_addons() {
     chroot /host rm -f "$install_dir/$(basename "$kernel_src")"
      
     # Restore config backup
-    local config_file="/etc/kata-containers/kata-se/configuration.toml"
-    local backup=$(ls -t "${config_file}.backup-"* 2>/dev/null | head -1)
-    [ -n "$backup" ] && [ -f "$backup" ] && chroot /host cp "$backup" "$config_file"
+#    local config_file="/etc/kata-containers/kata-se/configuration.toml"
+#    local backup=$(ls -t "${config_file}.backup-"* 2>/dev/null | head -1)
+#    [ -n "$backup" ] && [ -f "$backup" ] && chroot /host cp "$backup" "$config_file"
     
     echo "Addon artifacts uninstalled"
     return 0
